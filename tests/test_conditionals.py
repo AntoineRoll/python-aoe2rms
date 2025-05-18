34594341d9cc02a12194689b96dab61153e6a8e0
AntoineRoll <antoine-rollet@live.fr>
2025-05-18 13:36:03 +0200

    fix descrptions

--- tests/test_conditionals.py
+++ tests/test_conditionals.py
@@ -1,4 +1,4 @@
-from aoe2rms.conditional import ConditionalConstant, Variable
+from aoe2rms.conditionals import ConditionalConstant, Variable
 from aoe2rms.constants import TerrainConstant
 
 
@@ -15,7 +15,7 @@ def test_variable_with_prefix():
 def test_minimal_condition():
     conditional_script = ConditionalConstant(
         first_condition="TINY_MAP",
-        first_output=TerrainConstant(id=3, name="TEST_TERRAIN"),
+        first_output=TerrainConstant(id=3, name="TEST_TERRAIN", description="Dirt 3"),
     )
 
     assert (
@@ -27,14 +27,14 @@ def test_minimal_condition():
 def test_additional_conditions():
     conditional_script = ConditionalConstant(
         first_condition="TINY_MAP",
-        first_output=TerrainConstant(id=3, name="TEST_TERRAIN"),
+        first_output=TerrainConstant(id=3, name="TEST_TERRAIN", description="Dirt 3"),
         additional_conditions=[
-            ("SMALL_MAP", TerrainConstant(id=4, name="TEST_TERRAIN")),
-            ("MEDIUM_MAP", TerrainConstant(id=5, name="TEST_TERRAIN")),
-            ("LARGE_MAP", TerrainConstant(id=6, name="TEST_TERRAIN")),
-            ("HUGE_MAP", TerrainConstant(id=7, name="TEST_TERRAIN")),
-            ("GIGANTIC_MAP", TerrainConstant(id=8, name="TEST_TERRAIN")),
-            ("LUDIKRIS_MAP", TerrainConstant(id=9, name="TEST_TERRAIN")),
+            ("SMALL_MAP", TerrainConstant(id=4, name="TEST_TERRAIN", description="Shallows")),
+            ("MEDIUM_MAP", TerrainConstant(id=5, name="TEST_TERRAIN", description="Underbrush")),
+            ("LARGE_MAP", TerrainConstant(id=6, name="TEST_TERRAIN", description="Dirt 1")),
+            ("HUGE_MAP", TerrainConstant(id=7, name="TEST_TERRAIN", description="Farm")),
+            ("GIGANTIC_MAP", TerrainConstant(id=8, name="TEST_TERRAIN", description="Farm, Dead")),
+            ("LUDIKRIS_MAP", TerrainConstant(id=9, name="TEST_TERRAIN", description="Grass 3")),
         ],
     )
 
@@ -62,8 +62,8 @@ endif
 def test_else_conditions():
     conditional_script = ConditionalConstant(
         first_condition="TINY_MAP",
-        first_output=TerrainConstant(id=3, name="TEST_TERRAIN"),
-        else_output=TerrainConstant(id=4, name="TEST_TERRAIN"),
+        first_output=TerrainConstant(id=3, name="TEST_TERRAIN", description="Dirt 3"),
+        else_output=TerrainConstant(id=4, name="TEST_TERRAIN", description="Shallows"),
     )
 
     assert (
@@ -80,16 +80,16 @@ endif
 def test_full_conditions():
     conditional_script = ConditionalConstant(
         first_condition="TINY_MAP",
-        first_output=TerrainConstant(id=3, name="TEST_TERRAIN"),
+        first_output=TerrainConstant(id=3, name="TEST_TERRAIN", description="Dirt 3"),
         additional_conditions=[
-            ("SMALL_MAP", TerrainConstant(id=4, name="TEST_TERRAIN")),
-            ("MEDIUM_MAP", TerrainConstant(id=5, name="TEST_TERRAIN")),
-            ("LARGE_MAP", TerrainConstant(id=6, name="TEST_TERRAIN")),
-            ("HUGE_MAP", TerrainConstant(id=7, name="TEST_TERRAIN")),
-            ("GIGANTIC_MAP", TerrainConstant(id=8, name="TEST_TERRAIN")),
-            ("LUDIKRIS_MAP", TerrainConstant(id=9, name="TEST_TERRAIN")),
+            ("SMALL_MAP", TerrainConstant(id=4, name="TEST_TERRAIN", description="Shallows")),
+            ("MEDIUM_MAP", TerrainConstant(id=5, name="TEST_TERRAIN", description="Underbrush")),
+            ("LARGE_MAP", TerrainConstant(id=6, name="TEST_TERRAIN", description="Dirt 1")),
+            ("HUGE_MAP", TerrainConstant(id=7, name="TEST_TERRAIN", description="Farm")),
+            ("GIGANTIC_MAP", TerrainConstant(id=8, name="TEST_TERRAIN", description="Farm, Dead")),
+            ("LUDIKRIS_MAP", TerrainConstant(id=9, name="TEST_TERRAIN", description="Grass 3")),
         ],
-        else_output=TerrainConstant(id=10, name="OTHER_TERRAIN"),
+        else_output=TerrainConstant(id=10, name="OTHER_TERRAIN", description="Forest, Oak"),
     )
 
     assert (
@@ -119,12 +119,12 @@ def test_several_outputs():
     conditional_script = ConditionalConstant(
         first_condition="TINY_MAP",
         first_output=[
-            TerrainConstant(id=6, name="TEST_TERRAIN"),
-            TerrainConstant(id=7, name="SECOND_TERRAIN"),
+            TerrainConstant(id=6, name="TEST_TERRAIN", description="Dirt 1"),
+            TerrainConstant(id=7, name="SECOND_TERRAIN", description="Farm"),
         ],
         else_output=[
-            TerrainConstant(id=4, name="TEST_TERRAIN"),
-            TerrainConstant(id=5, name="SECOND_TERRAIN"),
+            TerrainConstant(id=4, name="TEST_TERRAIN", description="Shallows"),
+            TerrainConstant(id=5, name="SECOND_TERRAIN", description="Underbrush"),
         ],
     )
 
